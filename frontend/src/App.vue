32693d6eeaf61facf2b3f440368d45e08c80d2e5
IID4II <ramirezmonroyd@gmail.com>
2021-07-01 14:23:15 -0500

    Created a vue project folder

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <scoreboard />
+  </div>
+</template>
+
+<script>
+import Scoreboard from "./components/scoreboard";
+
+export default {
+  name: "App",
+  components: {
+    Scoreboard,
+  },
+};
+</script>
+
+<style></style>
---
+++ frontend/src/components/scoreboard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="scoreboard">holi</div>
+</template>
+
+<script>
+export default {
+  name: "Scoreboard",
+};
+</script>
+
+<style></style>
